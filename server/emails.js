688f28a8b21003692313cb05e9dbf561361aed56
leolower <leolower@gmail.com>
2018-08-27 11:58:31 -0300

    Send link to delete Alert on first email

--- server/emails.js
+++ server/emails.js
@@ -1,6 +1,10 @@
 var apiKey = process.env.MAILGUN_APIKEY
+var apiURL = process.env.API_URL
 if (!apiKey) {
   throw new Error('MAILGUN_APIKEY not configured. Add it to .env')
+}
+if (!apiURL) {
+  throw new Error('API_URL not configured. Add it to .env')
 }
 var domain = 'mg.cdpalert.org'
 var mailgun = require('mailgun-js')({
@@ -8,7 +12,16 @@ var mailgun = require('mailgun-js')({
   domain
 })
 
-function sendAlertRegistrationEmail ({ email, cdps, address, min, max }) {
+function sendAlertRegistrationEmail ({
+  email,
+  cdps,
+  address,
+  min,
+  max,
+  id,
+  secret
+}) {
+  const deleteLink = `${apiURL}/alerts/${id}/delete/${secret}`
   const text = `A new alert has been created with this email address (${email}).
 
     Details:
@@ -18,7 +31,7 @@ function sendAlertRegistrationEmail ({ email, cdps, address, min, max }) {
          - Min: ${min}%
          - Max: ${max}%
 
-    To delete this alert follow this link: TBD
+    To delete this alert follow this link: ${deleteLink}
 
     CDP Alert is an Open Source application. Made with ❤ by Protofire.io.
     `
@@ -29,6 +42,7 @@ function sendAlertRegistrationEmail ({ email, cdps, address, min, max }) {
     text
   }
   if (process.env.NODE_ENV !== 'production') {
+    console.log('Email data', data)
     return Promise.resolve('OK but email not sent in dev mode.')
   }
   return new Promise(function (resolve, reject) {
