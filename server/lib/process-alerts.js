3120af47e4960315df9d785f41f3d3e66775ded5
leolower <leolower@gmail.com>
2018-09-05 15:47:25 -0300

    Alert only once per day

--- server/lib/process-alerts.js
+++ server/lib/process-alerts.js
@@ -8,8 +8,13 @@ module.exports = {
 
 async function processAlerts (days = 60) {
   console.log('Processing alerts')
+  const yesterday = new Date()
+  yesterday.setDate(yesterday.getDate() - 1)
   const alerts = await Alert.find({
-    disabled: false
+    disabled: false,
+    lastNotifiedAt: {
+      $lt: yesterday
+    }
   })
   for (let i = 0; i < alerts.length; i++) {
     const alert = alerts[i]
@@ -18,26 +23,30 @@ async function processAlerts (days = 60) {
       const cdp = cdps[j]
       if (shouldAlertMin(alert, cdp)) {
         await notifyAlertMin(alert, cdp)
+        await updateAlertNotificationDate(alert, Date.now())
       }
       if (shouldAlertMax(alert, cdp)) {
         await notifyAlertMax(alert, cdp)
+        await updateAlertNotificationDate(alert, Date.now())
       }
     }
   }
   console.log(alerts.length, 'alerts processed')
 }
 
-// @TODO: get real CDPs
 async function getCDPs (ids) {
   return Promise.all(ids.map(getCDP))
 }
 
-// @TODO: check if collateralizationRatio is updated with ETH price
 function shouldAlertMin (alert, cdp) {
   return cdp.collateralizationRatio * 100 <= alert.min
 }
 
-// @TODO: check if collateralizationRatio is updated with ETH price
 function shouldAlertMax (alert, cdp) {
   return cdp.collateralizationRatio * 100 >= alert.max
+}
+
+async function updateAlertNotificationDate (alert, date) {
+  alert.lastNotifiedAt = new Date(date)
+  return alert.save()
 }
