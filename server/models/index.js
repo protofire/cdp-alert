ae28fd228f3c00a3ecd4a0a93d948cdaa1af703a
Sebastián Galiano <sgaliano@gmail.com>
2018-09-05 14:42:44 -0300

    Remove deprecation warnings with latest version of mongoose

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,8 @@
+const mongoose = require('./connection')
+
+mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true)
+
 module.exports = {
   Alert: require('./alert'),
   Cdp: require('./cdp'),
