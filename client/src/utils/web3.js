869b061d2324651df946772fda561bb9093ce60c
Mariano Aguero <mariano.aguero@altoros.com>
2018-11-22 14:12:59 -0300

    Add support to accounts not exposed by default

--- client/src/utils/web3.js
+++ client/src/utils/web3.js
@@ -6,18 +6,50 @@ export const Web3States = {
   OK: "Ok"
 };
 
+const returnWeb3 = async () => {
+    window.web3 = new Web3(window.web3.currentProvider);
+    const accounts = await window.web3.eth.getAccounts();
+    if (!accounts || !accounts.length) {
+        return { res: Web3States.NoAccount };
+    }
+
+    const networkId = await window.web3.eth.net.getId();
+
+    return {
+      res: Web3States.OK,
+      networkId: networkId,
+      account: accounts[0],
+      web3: window.web3
+    };
+};
+
+const returnNoWeb3 = () => {
+    return {
+        res: Web3States.NoWeb3
+    };
+};
+
 export const web3Checker = async function() {
-  if (!window.web3 || !window.web3.eth) {
-    return { res: Web3States.NoWeb3 };
-  }
 
-  const web3 = new Web3(window.web3.currentProvider);
-  const accounts = await web3.eth.getAccounts();
-  if (!accounts || !accounts.length) {
-    return { res: Web3States.NoAccount };
-  }
-
-  const networkId = await web3.eth.net.getId();
-
-  return { res: Web3States.OK, networkId, account: accounts[0], web3 };
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        try {
+            // Request account access if needed
+            await window.ethereum.enable();
+            return await returnWeb3();
+        } catch (error) {
+            // User denied account access...
+            return returnNoWeb3();
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+        return await returnWeb3();
+    }
+    // Non-dapp browsers...
+    else {
+        return returnNoWeb3();
+    }
 };
